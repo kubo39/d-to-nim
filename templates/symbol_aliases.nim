98f70085b2ebd98f9cb74e71a0516cf49b7490ec
Hiroki Noda <kubo39@gmail.com>
2023-03-22 08:09:17 +0900

    Add typed pattern for template

--- templates/symbol_aliases.nim
+++ templates/symbol_aliases.nim
@@ -2,12 +2,25 @@
 #
 #  nim c -r --verbosity:0 templates/symbol_aliases.nim
 
+# untyped pattern.
 template Foo(x: untyped, body: untyped) =
-    proc inc = x += 1
+    proc incriment = inc x
     block:
         body
 
-var v = 4
-Foo(v):
-    inc()
-doAssert v == 5
+block:
+    var v = 4
+    Foo(v):
+        incriment()
+    doAssert v == 5
+
+
+# typed pattern.
+template Bar(x: typed) =
+    proc incriment = inc x
+
+block:
+    var v = 4
+    Bar(v)
+    incriment()
+    doAssert v == 5
