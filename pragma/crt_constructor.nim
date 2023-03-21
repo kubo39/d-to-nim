3415d5f3beade001d0e8b56e3b6047c86353f49a
Hiroki Noda <kubo39@gmail.com>
2023-03-22 07:27:13 +0900

    Use exportc version

--- pragma/crt_constructor.nim
+++ pragma/crt_constructor.nim
@@ -6,8 +6,7 @@ var initCount: int
 
 # https://nim-lang.org/docs/manual.html#implementation-specific-pragmas-codegendecl-pragma
 #  * Add exportc pragma to avoid optimized-out.
-proc initializer() {.codegenDecl: "NIM_POSIX_INIT $# $#$#".} =
+proc initializer() {.exportc, codegenDecl: "NIM_POSIX_INIT $# $#$#".} =
   initCount += 1
 
-initializer()
-doAssert initCount == 2 # because initializer proc is called before C's main function.
+doAssert initCount == 1 # because initializer proc is called before C's main function.
