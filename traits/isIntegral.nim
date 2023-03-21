1eaf9418904902803f6501c0a73df6fc39d601c9
Hiroki Noda <kubo39@gmail.com>
2023-03-22 02:43:50 +0900

    Corresponding to isIntegral trait

---
+++ traits/isIntegral.nim
@@ -0,0 +1,30 @@
+# https://dlang.org/spec/traits.html#isIntegral
+#
+# nim c --verbosity:0 traits/isIntegral.nim
+
+var i: int
+var f: float
+type t = int
+var x: t
+type E = enum A, B
+
+static:
+    doAssert int is SomeInteger
+    doAssert i is SomeInteger
+    doAssert float isnot SomeInteger
+    doAssert f isnot SomeInteger
+    doAssert t is SomeInteger
+    doAssert x is SomeInteger
+    doAssert A isnot SomeInteger
+    doAssert bool isnot SomeInteger
+
+# `SomeOrdinal` is more like `isIntegral`.
+static:
+    doAssert int is SomeOrdinal
+    doAssert i is SomeOrdinal
+    doAssert float isnot SomeOrdinal
+    doAssert f isnot SomeOrdinal
+    doAssert t is SomeOrdinal
+    doAssert x is SomeOrdinal
+    doAssert A is SomeOrdinal
+    doAssert bool is SomeOrdinal
